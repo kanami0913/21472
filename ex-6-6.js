8ea9f362ab33159510f5e30d413ccf5507f7be97
kanami0913 <131091876+kanami0913@users.noreply.github.com>
2023-10-17 10:26:37 +0900

    Update ex-6-6.js

--- ex-6-6.js
+++ ex-6-6.js
@@ -1,5 +1,5 @@
 var who65='world';
-function sayhello65(){alert('Hello,'+who66+'!');}
+function sayhello66(){alert('Hello,'+who66+'!');}
 function taro065(){alert('太郎が来た');}
 function hanako065(){alert('花子が来た.');}
 function taro66(){who66='太郎';}
