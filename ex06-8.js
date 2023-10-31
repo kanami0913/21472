f95c3e3d45b08be07495fe2443bb50436aea4263
kanami0913 <131091876+kanami0913@users.noreply.github.com>
2023-10-31 09:38:34 +0900

    Create ex06-8.js

---
+++ ex06-8.js
@@ -0,0 +1,4 @@
+function sayhello68(){alert('Hello,'+document.getElementByld('who68').innerHTML+'!');}
+document.getElementByld('who68').tnnerHTML='太郎';}
+function hanako68(){document.getElementByld('who68').innerHTML='花子';
+                    
