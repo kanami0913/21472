3986dbf7364fae860f3eef9132227b06b3c0d9fd
kanami0913 <131091876+kanami0913@users.noreply.github.com>
2023-11-07 10:04:12 +0900

    Update ex.6-88.js

--- ex.6-88.js
+++ ex.6-88.js
@@ -1,3 +1,3 @@
 function gazo688() {
-  document.getElementById('who688').innerHTML = '<img src="./image/hana_ki.png" alt="オン"　hight="700">;
+  document.getElementById('who688').innerHTML = '<img src="./image/hana_ki.png" alt="オン"　height="500">;
 }
