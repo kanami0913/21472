f07b1261dc22287a02eaf1821af10478dd0ede74
kanami0913 <131091876+kanami0913@users.noreply.github.com>
2023-12-05 10:07:05 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,5 +1,9 @@
-function imgchange1(){
-  document.getElementById('kuma').src ='./image/bear.jpg';}
+function imgchange1() {
+document.getElementById('kuma').src = 
+'./image/bear.jpg';
+}
 
-function imgchange2(){
-  document.getElementById('kuma').src ='./image/kuma.png';}
+function imgchange2() {
+document.getElementById('kuma').src = 
+'./image/kuma.png';
+}
