eb7d5375ab793de02ea3ca37ec29d0cfd92777b9
kanami0913 <131091876+kanami0913@users.noreply.github.com>
2023-12-05 10:17:48 +0900

    Update ex6-10-2.js

--- ex6-10-2.js
+++ ex6-10-2.js
@@ -1,10 +1,3 @@
-function imgchange3(){
-  document.getElementById('logo').src =
-    './image/josai.png';
-}
-
-function imgchange4(){
-  document.getElementById('logo').src =
-    './image/ジョー君.jpg';
-}
+function imgchange3(){document.getElementById('logo').src ='./image/josai.png';}
+function imgchange4(){document.getElementById('logo').src ='./image/ジョー君.jpg';}
 
